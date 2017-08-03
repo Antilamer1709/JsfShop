887ad78dcbfb88f1a4679bc119f774c1e0da1646
vsloboda <vsloboda>
2017-08-04 00:10:40 +0200

    Styles added

---
+++ src/main/java/com/antilamer/jsfShop/bean/CommonBean.java
@@ -0,0 +1,18 @@
+package com.antilamer.jsfShop.bean;
+
+import com.antilamer.jsfShop.entity.UserEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@ManagedBean
+@RequestScoped
+public class CommonBean {
+
+}
