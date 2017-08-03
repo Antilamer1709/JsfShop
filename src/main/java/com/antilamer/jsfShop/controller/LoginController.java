03c91ca1c4b3b8ac9d40ca825aac469e2ca284ee
vsloboda <vsloboda>
2017-08-03 22:52:50 +0200

    Visual changes

--- src/main/java/com/antilamer/jsfShop/controller/LoginController.java
+++ src/main/java/com/antilamer/jsfShop/controller/LoginController.java
@@ -1,15 +1,9 @@
 package com.antilamer.jsfShop.controller;
 
-import com.antilamer.jsfShop.entity.UserEntity;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 @Scope(value = "session")
 @Component(value = "loginController")
